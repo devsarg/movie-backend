1433b5915344dbb9ce7f9dd06f756780b12c7d40
pablo <pabloservile@gmail.com>
2021-04-19 18:52:27 -0300

    first commit

---
+++ movies/src/test/java/com/devsarg/MoviesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsarg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
