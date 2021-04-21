d69acc7c27884a9994d30289d557ef6fb3ce73f2
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-04-20 21:10:27 -0300

    <fix> [service] : remove partial code

--- src/main/java/com/devsarg/service/UserService.java
+++ src/main/java/com/devsarg/service/UserService.java
@@ -1,14 +1,12 @@
 package com.devsarg.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class UserService {
-
-    @Autowired
-
+	
+	
 
 }
